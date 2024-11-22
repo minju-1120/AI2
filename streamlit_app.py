afac54f4f0ef83c35a24b7a5315d9f40b84a038a
minju-1120 <20242806@sdhs.gwe.hs.kr>
2024-11-22 14:57:22 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -44,12 +44,12 @@ def display_right_content(labels):
     # 1st Row - Images based on labels
     for i, label in enumerate(labels[:3]):
         with cols[i]:
-            st.image(f"https://via.placeholder.com/150?text={label}", caption=f"이미지: {label}", use_column_width=True)
+            st.image(f"https://i.ibb.co/5sKHZnK/image.jpg?text={label}", caption=f"이미지: {label}", use_column_width=True)
 
     # 2nd Row - YouTube Videos based on labels
     for i, label in enumerate(labels[:3]):
         with cols[i]:
-            st.video("https://www.youtube.com/watch?v=3JZ_D3ELwOQ", start_time=0)
+            st.video("https://youtu.be/SjJMSCHw1BQ?si=gll5Y01_YeCCh4QE", start_time=0)
             st.caption(f"유튜브: {label}")
 
     # 3rd Row - Text based on labels
